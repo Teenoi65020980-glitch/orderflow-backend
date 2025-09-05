9bd6cd3ffb2e6863b2313204c1698ee3d350aa7f
Teenoi65020980-glitch <nantateenoi6788@gmail.com>
2025-09-05 15:01:34 +0700

    server.ts

---
+++ server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = express()
+const prisma = new PrismaClient()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/products', async (req, res) => {
+  const products = await prisma.products.findMany()
+  res.json(products)
+})
+
+app.listen(5555, () => {
+  console.log('🚀 Server running at http://localhost:5555')
+})
